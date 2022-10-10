6ba14dd683791a970d69158c92c2e458f8d4c929
AnZhen <599002514@qq.com>
2022-10-10 20:27:50 +0800

    feat:项目初始化

---
+++ customResourceManager/src/main/java/com/crm/CustomResourceManagerApplication.java
@@ -0,0 +1,12 @@
+package com.crm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomResourceManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomResourceManagerApplication.class, args);
+    }
+}
